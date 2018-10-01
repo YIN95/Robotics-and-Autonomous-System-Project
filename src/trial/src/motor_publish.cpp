bd57a0b09a37b858c0425259a3d84bed8d05ef87
YIN95 <yinw@kth.se>
2018-10-01 19:16:52 +0200

    modify the code

--- src/trial/src/motor_publish.cpp
+++ src/trial/src/motor_publish.cpp
@@ -7,185 +7,58 @@
 
 class MotorController 
 {
-	public:
-		
-		ros::NodeHandle nh;
-		
-		// Subscribers
-		ros::Subscriber sub_velocity;
-		ros::Subscriber sub_encoder_left;
-		ros::Subscriber sub_encoder_right;
-	
-		// Publishers
-		ros::Publisher pub_left;
-		ros::Publisher pub_right;
-	
-		// Constructor
-		MotorController() {
-			
-			nh = ros::NodeHandle("~");
-			
-			sub_velocity = nh.subscribe<geometry_msgs::Twist>("/motor_controller/velocity", 1, &MotorController::velocityCallback, this);
-			sub_encoder_left = nh.subscribe<phidgets::motor_encoder>("/motor_left/encoder", 1, &MotorController::encoderCallbackLeft, this);
-			sub_encoder_right = nh.subscribe<phidgets::motor_encoder>("/motor_right/encoder", 1, &MotorController::encoderCallbackRight, this);
-	
-			pub_left = nh.advertise<std_msgs::Float32>("/motor_left/cmd_vel", 1);
-			pub_right = nh.advertise<std_msgs::Float32>("/motor_right/cmd_vel", 1);
-			
-			int control_frequency = 10;
-			dt = 1.0 / control_frequency;
-			wheel_radius = 0.05;
-			base = 0.19;
-			ticks_per_rev = 360;
-			LEFT = 0;
-			RIGHT = 1;
-			
-			alpha = std::vector<double>(2, 0);
-			beta = std::vector<double>(2, 0);
-			alpha[LEFT] = 10;
-			alpha[RIGHT] = 10;
-			beta[LEFT] = 5;
-			beta[RIGHT] = 5;
-			int_error = std::vector<double>(2, 0);
-			w_desired =	std::vector<double>(2, 0);
-			w_estimate = std::vector<double>(2, 0);
-			delta_encoder =	std::vector<int>(2, 0);
-	
-		}
-		
-		MotorController(int control_frequency_) {
-		
-			nh = ros::NodeHandle("~");
-		
-			sub_velocity = nh.subscribe<geometry_msgs::Twist>("/motor_controller/velocity", 1, &MotorController::velocityCallback, this);
-			sub_encoder_left = nh.subscribe<phidgets::motor_encoder>("/motor_left/encoder", 1, &MotorController::encoderCallbackLeft, this);
-			sub_encoder_right = nh.subscribe<phidgets::motor_encoder>("/motor_right/encoder", 1, &MotorController::encoderCallbackRight, this);
-	
-			pub_left = nh.advertise<std_msgs::Float32>("/motor_left/cmd_vel", 1);
-			pub_right = nh.advertise<std_msgs::Float32>("/motor_right/cmd_vel", 1);
-			
-			control_frequency = control_frequency_;
-			dt = 1.0 / control_frequency;
-			wheel_radius = 0.05;
-			base = 0.19;
-			ticks_per_rev = 360;
-			LEFT = 0;
-			RIGHT = 1;
-			
-			alpha = std::vector<double>(2, 0);
-			beta = std::vector<double>(2, 0);
-			alpha[LEFT] = 20;
-			alpha[RIGHT] = 20;
-			beta[LEFT] = 0;
-			beta[RIGHT] = 0;
-			int_error = std::vector<double>(2, 0);
-			w_desired =	std::vector<double>(2, 0);
-			w_estimate = std::vector<double>(2, 0);
-			delta_encoder =	std::vector<int>(2, 0);
-	
-		}
-		
-		void velocityCallback(const geometry_msgs::Twist::ConstPtr &msg) {
-			v_robot_desired = msg->linear.x;
-			w_robot_desired= msg->angular.z;
-		}
-		
-		void encoderCallbackLeft(const phidgets::motor_encoder::ConstPtr &msg) {
-			delta_encoder[LEFT] = msg->count_change;
-			ROS_INFO("enc left : %i", delta_encoder[LEFT]);
-		}
-		
-		void encoderCallbackRight(const phidgets::motor_encoder::ConstPtr &msg) {
-			delta_encoder[RIGHT] = msg->count_change;
-			ROS_INFO("enc right : %i", delta_encoder[RIGHT]);
-		}
+public: /* ros */
+	ros::NodeHandle nh;
+	/* Subscribers */
+	ros::Subscriber sub_velocity;
+	ros::Subscriber sub_encoder_left;
+	ros::Subscriber sub_encoder_right;
+	/* Publishers */
+	ros::Publisher pub_left;
+	ros::Publisher pub_right;
 
-		void updateEstimatedSpeed() {
-			w_estimate[LEFT] = (delta_encoder[LEFT] * 2 * M_PI * control_frequency) / ticks_per_rev;
-			w_estimate[RIGHT] = (delta_encoder[RIGHT] * 2 * M_PI * control_frequency) / ticks_per_rev;
-			w_estimate[RIGHT] *= -1; // compensate for right wheel spinning in wrong direction
+public: /* Functions */
+	/* Constructor Functions */
+	MotorController();
+	MotorController(int control_frequency_);
+
+	/* Functions */
+	void velocityCallback(const geometry_msgs::Twist::ConstPtr &msg);
+	void encoderCallbackLeft(const phidgets::motor_encoder::ConstPtr &msg);
+	void encoderCallbackRight(const phidgets::motor_encoder::ConstPtr &msg);
+
+	void updateEstimatedSpeed();
+	void updateDesiredSpeed();
+
+	void setMotorPowers();
+	void clipPowerValues();
+	void PI();
+
+private:
+	/* Variables */
+	const static int LEFT = 0;	// LEFT SIDE
+	const static int RIGHT = 1;	// RIGHT SIDE
+	const static double base = 0.19;
+	const static double wheel_radius = 0.05;
+	const static int ticks_per_rev = 360;
+
+	int control_frequency;
+	double dt;
 	
-			ROS_INFO("w est left : %f", w_estimate[LEFT]);
-			ROS_INFO("w est right: %f", w_estimate[RIGHT]);
-		}
+	double v_robot_desired;
+	double w_robot_desired;
 
-		void updateDesiredSpeed() {
-			w_desired[LEFT] = (v_robot_desired - w_robot_desired * (base / 2)) / wheel_radius;
-			w_desired[RIGHT] = (v_robot_desired + w_robot_desired * (base / 2)) / wheel_radius;
-	
-			ROS_INFO("w des left : %f", w_desired[LEFT]);
-			ROS_INFO("w des right: %f", w_desired[RIGHT]);
-		}
-		
-		void setMotorPowers() {
-			double error;
-			int signal;
-			
-			error = w_desired[LEFT] - w_estimate[LEFT];
-			int_error[LEFT] += error * dt;
-			ROS_INFO("error: %f", error);
-			signal = int (alpha[LEFT] * error + beta[LEFT] * int_error[LEFT]);
-			left_motor.data = signal;
-			
-			
-			error = w_desired[RIGHT] - w_estimate[RIGHT];
-			int_error[RIGHT] += error * dt;
-			ROS_INFO("error: %f", error);
-			signal = int (alpha[RIGHT] * error + beta[RIGHT] * int_error[RIGHT]);
-			right_motor.data = signal;
-		}
+	std_msgs::Float32 left_motor;
+	std_msgs::Float32 right_motor;
 
-		void clipPowerValues() {
-			// Also clip on negative values
-			double left = std::abs(left_motor.data);
-			double right = std::abs(right_motor.data);
-			double largest = std::max(left, right);
-			if (largest > 80) {
-				ROS_INFO("Clipping stuff");
-				double scale_factor = 80 / largest;
-				left_motor.data *= scale_factor;
-				right_motor.data *= scale_factor;
-			}
-			ROS_INFO("signal to left motor: %f", left_motor.data);
-			ROS_INFO("signal to right motor: %f", right_motor.data);
-		}
-		
-		void PI() {
-			updateDesiredSpeed();
-			updateEstimatedSpeed();
-			setMotorPowers();
-			clipPowerValues();
-			pub_left.publish(left_motor);
-			//pub_right.publish(right_motor);
-		}
-	
-	private:
-		int control_frequency;
-		double base;
-		double wheel_radius;
-		int ticks_per_rev;
-		double dt;
-
-		double v_robot_desired;
-		double w_robot_desired;
-
-		// First element represent the left side, use LEFT and RIGHT variables
-		int LEFT;
-		int RIGHT;
-		
-		std_msgs::Float32 left_motor;
-		std_msgs::Float32 right_motor;
-
-		std::vector<double> alpha;
-		std::vector<double> beta;
-		std::vector<double> int_error;
-		std::vector<double> w_desired;
-		std::vector<double> w_estimate;
-		std::vector<int> delta_encoder;
+	std::vector<double> alpha;
+	std::vector<double> beta;
+	std::vector<double> int_error;
+	std::vector<double> w_desired;
+	std::vector<double> w_estimate;
+	std::vector<int> delta_encoder;
 
 };
-
-
 int main(int argc, char** argv) 
 {	
 	const int control_frequency = 10;
@@ -201,4 +74,132 @@ int main(int argc, char** argv)
 		ROS_INFO("------------------------------");
 	}
 	return 0;
+}
+
+
+MotorController::MotorController(){
+			
+	nh = ros::NodeHandle("~");
+	
+	sub_velocity = nh.subscribe<geometry_msgs::Twist>("/motor_controller/velocity", 1, &MotorController::velocityCallback, this);
+	sub_encoder_left = nh.subscribe<phidgets::motor_encoder>("/motor_left/encoder", 1, &MotorController::encoderCallbackLeft, this);
+	sub_encoder_right = nh.subscribe<phidgets::motor_encoder>("/motor_right/encoder", 1, &MotorController::encoderCallbackRight, this);
+
+	pub_left = nh.advertise<std_msgs::Float32>("/motor_left/cmd_vel", 1);
+	pub_right = nh.advertise<std_msgs::Float32>("/motor_right/cmd_vel", 1);
+	
+	int control_frequency = 10;
+	dt = 1.0 / control_frequency;
+			
+	alpha = std::vector<double>(2, 0);
+	beta = std::vector<double>(2, 0);
+	alpha[LEFT] = 10;
+	alpha[RIGHT] = 10;
+	beta[LEFT] = 5;
+	beta[RIGHT] = 5;
+	int_error = std::vector<double>(2, 0);
+	w_desired =	std::vector<double>(2, 0);
+	w_estimate = std::vector<double>(2, 0);
+	delta_encoder =	std::vector<int>(2, 0);
+}
+
+MotorController::MotorController(int control_frequency_) {		
+	nh = ros::NodeHandle("~");
+
+	sub_velocity = nh.subscribe<geometry_msgs::Twist>("/motor_controller/velocity", 1, &MotorController::velocityCallback, this);
+	sub_encoder_left = nh.subscribe<phidgets::motor_encoder>("/motor_left/encoder", 1, &MotorController::encoderCallbackLeft, this);
+	sub_encoder_right = nh.subscribe<phidgets::motor_encoder>("/motor_right/encoder", 1, &MotorController::encoderCallbackRight, this);
+
+	pub_left = nh.advertise<std_msgs::Float32>("/motor_left/cmd_vel", 1);
+	pub_right = nh.advertise<std_msgs::Float32>("/motor_right/cmd_vel", 1);
+	
+	control_frequency = control_frequency_;
+	dt = 1.0 / control_frequency;
+	
+	alpha = std::vector<double>(2, 0);
+	beta = std::vector<double>(2, 0);
+	alpha[LEFT] = 20;
+	alpha[RIGHT] = 20;
+	beta[LEFT] = 0;
+	beta[RIGHT] = 0;
+	int_error = std::vector<double>(2, 0);
+	w_desired =	std::vector<double>(2, 0);
+	w_estimate = std::vector<double>(2, 0);
+	delta_encoder =	std::vector<int>(2, 0);
+}
+
+void MotorController::velocityCallback(const geometry_msgs::Twist::ConstPtr &msg) {
+	v_robot_desired = msg->linear.x;
+	w_robot_desired= msg->angular.z;
+}
+
+void MotorController::encoderCallbackLeft(const phidgets::motor_encoder::ConstPtr &msg) {
+	delta_encoder[LEFT] = msg->count_change;
+	ROS_INFO("enc left : %i", delta_encoder[LEFT]);
+}
+
+void MotorController::encoderCallbackRight(const phidgets::motor_encoder::ConstPtr &msg) {
+	delta_encoder[RIGHT] = msg->count_change;
+	ROS_INFO("enc right : %i", delta_encoder[RIGHT]);
+}
+
+void MotorController::updateEstimatedSpeed() {
+	w_estimate[LEFT] = (delta_encoder[LEFT] * 2 * M_PI * control_frequency) / ticks_per_rev;
+	w_estimate[RIGHT] = (delta_encoder[RIGHT] * 2 * M_PI * control_frequency) / ticks_per_rev;
+	w_estimate[RIGHT] *= -1; // compensate for right wheel spinning in wrong direction
+
+	ROS_INFO("w est left : %f", w_estimate[LEFT]);
+	ROS_INFO("w est right: %f", w_estimate[RIGHT]);
+}
+
+void MotorController::updateDesiredSpeed() {
+	w_desired[LEFT] = (v_robot_desired - w_robot_desired * (base / 2)) / wheel_radius;
+	w_desired[RIGHT] = (v_robot_desired + w_robot_desired * (base / 2)) / wheel_radius;
+
+	ROS_INFO("w des left : %f", w_desired[LEFT]);
+	ROS_INFO("w des right: %f", w_desired[RIGHT]);
+}
+
+void MotorController::setMotorPowers() {
+	double error;
+	int signal;
+	
+	error = w_desired[LEFT] - w_estimate[LEFT];
+	int_error[LEFT] += error * dt;
+	ROS_INFO("error: %f", error);
+	signal = int (alpha[LEFT] * error + beta[LEFT] * int_error[LEFT]);
+	left_motor.data = signal;
+	
+	
+	error = w_desired[RIGHT] - w_estimate[RIGHT];
+	int_error[RIGHT] += error * dt;
+	ROS_INFO("error: %f", error);
+	signal = int (alpha[RIGHT] * error + beta[RIGHT] * int_error[RIGHT]);
+	right_motor.data = signal;
+}
+
+void MotorController::clipPowerValues() {
+	double clip_value = 100;
+
+	// Also clip on negative values
+	double left = std::abs(left_motor.data);
+	double right = std::abs(right_motor.data);
+	double largest = std::max(left, right);
+	if (largest > clip_value) {
+		ROS_INFO("Clipping stuff");
+		double scale_factor = clip_value / largest;
+		left_motor.data *= scale_factor;
+		right_motor.data *= scale_factor;
+	}
+	ROS_INFO("signal to left motor: %f", left_motor.data);
+	ROS_INFO("signal to right motor: %f", right_motor.data);
+}
+
+void MotorController::PI() {
+	updateDesiredSpeed();
+	updateEstimatedSpeed();
+	setMotorPowers();
+	clipPowerValues();
+	pub_left.publish(left_motor);
+	pub_right.publish(right_motor);
 }
--- src/trial/src/velocity_publish.cpp
+++ src/trial/src/velocity_publish.cpp
@@ -1,7 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 
-
 double v = 0.1;
 double w = 0;
 
@@ -10,7 +9,7 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "velocity_publish");
 	ros::NodeHandle nh;
 	ros::Rate rate(10);
-	
+
 	ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/motor_controller/velocity", 1);
 	
 	geometry_msgs::Twist velocity_msg;
