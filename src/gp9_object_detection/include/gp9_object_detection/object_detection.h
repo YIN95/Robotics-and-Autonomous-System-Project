beb1d3f2b9543d6f4ca7f7640c79d59a68e0733a
YIN95 <yinw@kth.se>
2018-10-10 12:47:09 +0200

    rm realsense

---
+++ src/gp9_object_detection/include/gp9_object_detection/object_detection.h
@@ -0,0 +1,3 @@
+#include "ros/ros.h"
+#include <math.h>
+#include <vector>
---
+++ src/gp9_object_detection/src/object_detection.cpp
@@ -0,0 +1,9 @@
+#include "object_detection.h"
+
+int main(int argc, char** argv) 
+{	
+
+	ros::init(argc, argv, "object_detection");	
+
+	return 0;
+}
