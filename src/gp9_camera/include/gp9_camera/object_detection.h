fb4ba7a4365fce927fac5d460bcb87d47d48382a
YIN95 <yinw@kth.se>
2018-10-12 15:24:46 +0200

    added changes from pclbranch

--- src/gp9_camera/include/gp9_camera/object_detection.h
+++ src/gp9_camera/include/gp9_camera/object_detection.h
@@ -8,25 +8,33 @@
 class ObjectDetection{
 public: /* ros */
 	ros::NodeHandle nh;
-	/* Subscribers */
+	/* Subscribers and publishers */
 	ros::Subscriber sub_pointcloud;
+	ros::Publisher pub_pointcloud;
+
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr rgb_cloud;
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr res_cloud;
+    pcl::PointCloud<pcl::PointXYZHSV>::Ptr hsv_cloud;
+
 
 public: /* Functions */
 	/* Constructor Functions */
 	ObjectDetection();
 
 	void pointCloudCallback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &msg);
+	void pubCloud();
 	void detectObject();
 	void cleanData();
 	void clusterObject();
-	void checkColor();
+	void checkColor(int h);
+    void dimFilter(std::string dim, double lower_bound, double upper_bound, bool inv_cond);
+	void colorFilter(std::string dim, double lower_bound, double upper_bound, bool inv_cond);
+	void makeVoxelGrid();
+	void removeOutliers();
 	void checkSize();
-        bool objectInImage();
-
+	bool objectInImage();
+	void PointCloudXYZHSVtoXYZRGB(pcl::PointCloud<pcl::PointXYZHSV>& in, pcl::PointCloud<pcl::PointXYZRGB>& out);
+	void PointCloudXYZRGBtoXYZHSV(pcl::PointCloud<pcl::PointXYZRGB>& in, pcl::PointCloud<pcl::PointXYZHSV>& out);
 private:
-        pcl::PointCloud<pcl::PointXYZRGB> rgb_cloud;
-        pcl::PointCloud<pcl::PointXYZHSV> hsv_cloud;
         double color_lim_s;
-	unsigned int height;
-	unsigned int width;
 };
