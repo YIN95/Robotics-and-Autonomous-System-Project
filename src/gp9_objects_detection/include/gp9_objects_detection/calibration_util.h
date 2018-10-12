26ccebc805fdc176bfeb874216da36d99dd40126
YIN95 <yinw@kth.se>
2018-10-12 16:44:43 +0200

    added calibration

--- src/gp9_objects_detection/include/gp9_objects_detection/calibration_util.h
+++ src/gp9_objects_detection/include/gp9_objects_detection/calibration_util.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <std_msgs/Float32.h>
 #include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <cv_bridge/cv_bridge.h>
 #include <iostream>
 #include <stdio.h>
@@ -12,6 +13,8 @@ public:
     ros::NodeHandle nh;
     ros::Subscriber sub_image_rgb;
     ros::Subscriber sub_image_depth;
+    ros::Subscriber rgb_info_sub;
+    ros::Subscriber depth_info_sub;
 
 public:
     static const double fx = 619.7237548828125;
@@ -23,8 +26,21 @@ public:
     static const int image_height = 480;
     static const int image_width = 640;
 
+    float f_x_rgb; 
+    float f_y_rgb;
+    float c_x_rgb;
+    float c_y_rgb;
+
+    float f_x_depth;
+    float f_y_depth;
+    float c_x_depth;
+    float c_y_depth;
+
 public: 
     Calibration();
     void imageRGBCallback(const sensor_msgs::ImageConstPtr &msg);
     void imageDepthCallback(const sensor_msgs::ImageConstPtr &msg);
+    void RGBInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg);
+    void depthInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg);
+    void calcCoord();
 };
--- src/gp9_objects_detection/src/calibration_util.cpp
+++ src/gp9_objects_detection/src/calibration_util.cpp
@@ -5,22 +5,46 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "calibration_util");
 
     Calibration calibration;
+    ros::Rate rate(10);
 
     while(calibration.nh.ok()){
         ros::spinOnce();
+        calibration.calcCoord();
+        rate.sleep();
     }
 
     return 0;
 }
 
 Calibration::Calibration(){
+    rgb_info_sub = nh.subscribe<sensor_msgs::CameraInfo>("/camera/rgb/camera_info", 1, &Calibration::RGBInfoCallback, this);
+	depth_info_sub = nh.subscribe<sensor_msgs::CameraInfo>("/camera/depth/camera_info", 1, &Calibration::depthInfoCallback, this);
     sub_image_rgb = nh.subscribe<sensor_msgs::Image>("/camera/rgb/image_rect_color", 1, &Calibration::imageRGBCallback, this);
 	sub_image_depth = nh.subscribe<sensor_msgs::Image>("/camera/depth_registered/sw_registered/image_rect", 1, &Calibration::imageDepthCallback, this);
+}
+
+void Calibration::RGBInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg){
+    // P is row-major, so P is listed row by row
+    f_x_rgb = msg->P[0]; 
+    f_y_rgb = msg->P[5];
+    c_x_rgb = msg->P[2];
+    c_y_rgb = msg->P[6];
+}
+void Calibration::depthInfoCallback(const sensor_msgs::CameraInfo::ConstPtr &msg){
+    // P is row-major, so P is listed row by row
+    f_x_depth = msg->P[0];
+    f_y_depth = msg->P[5];
+    c_x_depth = msg->P[2];
+    c_y_depth = msg->P[6];
 }
 
 void Calibration::imageRGBCallback(const sensor_msgs::ImageConstPtr &msg){
 
 }
 void Calibration::imageDepthCallback(const sensor_msgs::ImageConstPtr &msg){
+    
+}
+
+void Calibration::calcCoord(){
     
 }
