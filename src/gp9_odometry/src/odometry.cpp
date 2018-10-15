c44ef8c06ed61584a194e0edd11724727d74bebd
hannefornander <hannes.fornander@hotmail.com>
2018-10-15 10:43:59 +0200

    detection more robust

--- src/gp9_odometry/src/odometry.cpp
+++ src/gp9_odometry/src/odometry.cpp
@@ -118,7 +118,7 @@ void Odometry::pub_robot_Pose(double x, double y){
     marker.scale.y = 0.1;
     marker.scale.z = 0.1;
     marker.color.a = 0.2; 
-    marker.color.r = 0.2;
+    marker.color.r = 1.0;
     marker.color.g = 0;
     marker.color.b = 1.0;
     // marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
