3a2e864d5933b0946731e419b66d961b49aa6aac
YIN95 <yinw@kth.se>
2018-11-29 17:08:15 +0100

    added changes to battery mapper

--- src/gp9_mapping/scripts/battery_mapper.py
+++ src/gp9_mapping/scripts/battery_mapper.py
@@ -4,6 +4,11 @@ from __future__ import print_function
 from __future__ import division
 
 import rospy
+import sys
+sys.path.append('/home/ras19/catkin_ws/src/gp9_path_planning/scripts/')
+
+import shapely.geometry as sh
+from obstacle_map import LineBox
 from geometry_msgs.msg import Pose2D
 from std_msgs.msg import Bool
 
@@ -15,9 +20,11 @@ class BatteryMapper:
         self.new_path = "/home/ras19/catkin_ws/src/gp9_path_planning/maps/updated_maze.txt"
         self._create_new_file()
         self.subscriber_battery = rospy.Subscriber("/findBattery", Pose2D, self.callback_battery)
-        self.publisher_battery = rospy.Publisher("/update_map", Bool)
+        self.publisher_battery = rospy.Publisher("/update_map", Bool, queue_size=1)
         self.dx = -0.01
         self.dy = -0.01
+        self.batteries = []
+        self.robot_radius = 0.16
 
     def _create_new_file(self):
         with open(self.maze_path) as f:
@@ -29,14 +36,29 @@ class BatteryMapper:
     def callback_battery(self, battery):
         x1 = battery.x
         y1 = battery.y
-        x2 = x1 + self.dx
-        y2 = y1 + self.dy
-        line = "\n%.2f %.2f %.2f %.2f" % (x1, y1, x2, y2)
 
-        with open(self.new_path, 'a+') as f:
-            f.write(line)
+        battery_pos = sh.Point(x1, y1)
+        battery_already_detected = False
+        for polygon in self.batteries:
+            if polygon.contains(battery_pos):
+                battery_already_detected = True
+                rospy.loginfo("Already detected this battery")
+                break
+        
+        if not battery_already_detected:
 
-        self.publisher_battery.publish(Bool(data=True))
+            x2 = x1 + self.dx
+            y2 = y1 + self.dy
+            line = "\n%.2f %.2f %.2f %.2f" % (x1, y1, x2, y2)
+            linebox = LineBox(sh.Point(x1, y1), sh.Point(x2, y2), self.robot_radius)
+            battery_polygon = sh.Polygon(linebox.ordered_vertices)
+
+            rospy.loginfo("writing battery position to file!")
+            with open(self.new_path, 'a') as f:
+                f.write(line)
+
+            self.batteries.append(battery_polygon)
+            self.publisher_battery.publish(Bool(data=True))
 
 
 if __name__ == '__main__':
