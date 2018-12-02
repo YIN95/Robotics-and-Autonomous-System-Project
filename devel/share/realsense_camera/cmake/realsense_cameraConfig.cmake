b2a57f69212fb993360768604e78590fa9142227
YIN95 <yinw@kth.se>
2018-12-02 14:33:01 +0100

    merge done

--- devel/share/realsense_camera/cmake/realsense_cameraConfig.cmake
+++ devel/share/realsense_camera/cmake/realsense_cameraConfig.cmake
@@ -67,14 +67,14 @@ set(realsense_camera_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(realsense_camera_SOURCE_PREFIX /home/ras29/catkin_ws/src/ras_project/ras_realsense/realsense_camera)
-  set(realsense_camera_DEVEL_PREFIX /home/ras29/catkin_ws/devel)
+  set(realsense_camera_SOURCE_PREFIX /home/ras19/catkin_ws/src/ras_project/ras_realsense/realsense_camera)
+  set(realsense_camera_DEVEL_PREFIX /home/ras19/catkin_ws/devel)
   set(realsense_camera_INSTALL_PREFIX "")
   set(realsense_camera_PREFIX ${realsense_camera_DEVEL_PREFIX})
 else()
   set(realsense_camera_SOURCE_PREFIX "")
   set(realsense_camera_DEVEL_PREFIX "")
-  set(realsense_camera_INSTALL_PREFIX /home/ras29/catkin_ws/install)
+  set(realsense_camera_INSTALL_PREFIX /home/ras19/catkin_ws/install)
   set(realsense_camera_PREFIX ${realsense_camera_INSTALL_PREFIX})
 endif()
 
@@ -91,9 +91,9 @@ endif()
 # flag project as catkin-based to distinguish if a find_package()-ed project is a catkin project
 set(realsense_camera_FOUND_CATKIN_PROJECT TRUE)
 
-if(NOT "/home/ras29/catkin_ws/devel/include;/home/ras29/catkin_ws/src/ras_project/ras_realsense/realsense_camera/include " STREQUAL " ")
+if(NOT "/home/ras19/catkin_ws/devel/include;/home/ras19/catkin_ws/src/ras_project/ras_realsense/realsense_camera/include " STREQUAL " ")
   set(realsense_camera_INCLUDE_DIRS "")
-  set(_include_dirs "/home/ras29/catkin_ws/devel/include;/home/ras29/catkin_ws/src/ras_project/ras_realsense/realsense_camera/include")
+  set(_include_dirs "/home/ras19/catkin_ws/devel/include;/home/ras19/catkin_ws/src/ras_project/ras_realsense/realsense_camera/include")
   if(NOT "https://github.com/intel-ros/realsense/issues " STREQUAL " ")
     set(_report "Check the issue tracker 'https://github.com/intel-ros/realsense/issues' and consider creating a ticket if the problem has not been reported yet.")
   elseif(NOT "http://www.ros.org/wiki/RealSense " STREQUAL " ")
@@ -110,7 +110,7 @@ if(NOT "/home/ras29/catkin_ws/devel/include;/home/ras29/catkin_ws/src/ras_projec
         message(FATAL_ERROR "Project 'realsense_camera' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'realsense_camera' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ras29/catkin_ws/src/ras_project/ras_realsense/realsense_camera/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'realsense_camera' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ras19/catkin_ws/src/ras_project/ras_realsense/realsense_camera/${idir}'.  ${_report}")
     endif()
     _list_append_unique(realsense_camera_INCLUDE_DIRS ${include})
   endforeach()
@@ -129,7 +129,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ras29/catkin_ws/devel/lib;/home/ras29/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/ras19/catkin_ws/devel/lib;/home/ras19/catkin_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
--- devel/share/uarm/cmake/uarm-msg-extras.cmake
+++ devel/share/uarm/cmake/uarm-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(uarm_MESSAGE_FILES "")
-set(uarm_SERVICE_FILES "/home/ras29/catkin_ws/src/ras_project/ras_uarm/srv/MoveTo.srv;/home/ras29/catkin_ws/src/ras_project/ras_uarm/srv/MoveToJoints.srv;/home/ras29/catkin_ws/src/ras_project/ras_uarm/srv/Pump.srv;/home/ras29/catkin_ws/src/ras_project/ras_uarm/srv/AttachDetach.srv")
+set(uarm_SERVICE_FILES "/home/ras19/catkin_ws/src/ras_project/ras_uarm/srv/MoveTo.srv;/home/ras19/catkin_ws/src/ras_project/ras_uarm/srv/MoveToJoints.srv;/home/ras19/catkin_ws/src/ras_project/ras_uarm/srv/Pump.srv;/home/ras19/catkin_ws/src/ras_project/ras_uarm/srv/AttachDetach.srv")
