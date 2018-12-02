0cb19591a4a34db5778e2b0526c40ce5e4d69f47
hannefornander <hannes.fornander@hotmail.com>
2018-12-02 14:19:14 +0100

    halfway through mapping of walls

--- devel/share/gp9_path_planning/cmake/gp9_path_planning-msg-extras.cmake
+++ devel/share/gp9_path_planning/cmake/gp9_path_planning-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(gp9_path_planning_MESSAGE_FILES "/home/ras19/catkin_ws/src/gp9_path_planning/msg/PathList.msg")
+set(gp9_path_planning_MESSAGE_FILES "/home/ras29/catkin_ws/src/gp9_path_planning/msg/PathList.msg")
 set(gp9_path_planning_SERVICE_FILES "")
